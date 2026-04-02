fd788f6d2e9b01a3d3814d69db402f3cf85363e1
Raj Meena <your-email@gmail.com>
2026-04-02 23:35:16 +0530

    second update

--- src/components/Daily/Chart.jsx
+++ src/components/Daily/Chart.jsx
@@ -8,6 +8,8 @@ import React, { useEffect, useRef, useState, useCallback } from "react";
 import { createChart, CandlestickSeries, createSeriesMarkers } from "lightweight-charts";
 import { CrosshairMode } from "lightweight-charts";
 import "./Markets.css";
+import "../../utils/constants"
+import { API_URL } from "../../utils/constants";
 
 const TF_MAP = { "1m": "1m", "5m": "5m", "15m": "15m", "1h": "1h" };
 const INTERVAL_MS = { "1m": 60*1000, "5m": 5*60*1000, "15m": 15*60*1000, "1h": 60*60*1000 };
@@ -153,8 +155,7 @@ const cleanedSymbol = mapToBinanceSymbol(cleanSymbol(symbol));
       let endTime = showFullDay && tradeDate ? getDateRange().endTime : Date.now();
 
       for (let i=0; i<requestsNeeded; i++) {
-        // const url = `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${limit}&endTime=${endTime}`;
-        const url = `http://localhost:5000/api/klines?symbol=${symbol}&interval=${interval}&limit=${limit}&endTime=${endTime}`;
+        const url = `${API_URL}/api/klines?symbol=${symbol}&interval=${interval}&limit=${limit}&endTime=${endTime}`;
         const res = await fetch(url);
         const data = await res.json();
         allCandles = [...data, ...allCandles];
